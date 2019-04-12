a3fbd88e692f370138e82f75dfc62c2c90b60136
Brad <bturner1273@gmail.com>
2019-04-11 20:33:04 -0400

    everything minus google auth is done

--- web/assets/js/sign_in.js
+++ web/assets/js/sign_in.js
@@ -1,16 +1,80 @@
-window.onload = function () {
+$(document).ready(function () {
+// FIREBASE INIT
+    var config = {
+        apiKey: "AIzaSyDi8KqMpIHqShxR2WJZO3eto5BL2mnObl0",
+        authDomain: "smart-air-810ae.firebaseapp.com",
+        databaseURL: "https://smart-air-810ae.firebaseio.com",
+        projectId: "smart-air-810ae",
+        storageBucket: "smart-air-810ae.appspot.com",
+        messagingSenderId: "495437676642"
+    };
+    firebase.initializeApp(config);
+// END FIREBASE INIT
 
-    $("#show_password").click(function () {
-        var i_tag = $(this).find("i").get(0);
-        var i_tag_class = $(i_tag).attr("class");
-        if (i_tag_class === "fal fa-eye") {
-            $(i_tag).attr("class", "fal fa-eye-slash");
-            $("#password").attr("type", "password");
-        }else {
-            $("#password").attr("type", "text");
-            $(i_tag).attr("class", "fal fa-eye");
-        }
+//binds the login form to submit on enter key press in the password text input
+    addInputSubmitEvent($("#sign_up_form"), $("#sign_up_password"));
+    addInputSubmitEvent($("#sign_in_form"), $("#password"));
+    bind_show_password("show_password","password");
+    bind_show_password("sign_up_show_password", "sign_up_password");
+
+    //toggles whether password will show or not
+    function bind_show_password(button_id, text_input_id) {
+        $("#"+button_id).click(function () {
+            var i_tag = $(this).find("i").get(0);
+            var i_tag_class = $(i_tag).attr("class");
+            if (i_tag_class === "fal fa-eye") {
+                $(i_tag).attr("class", "fal fa-eye-slash");
+                $("#"+text_input_id).attr("type", "password");
+            }else {
+                $("#"+text_input_id).attr("type", "text");
+                $(i_tag).attr("class", "fal fa-eye");
+            }
+        });
+    }
+
+    $("#sign_in_form").on("submit", function (event) {
+        event.preventDefault();
+        var email = $("#email").val();
+        var password = $("#password").val();
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(
+            function (user) {
+                console.log(user);
+            }
+        )
+        .catch(
+            function (err) {
+                console.log(err);
+            }
+        )
     });
 
+    $("#sign_up_form").on("submit", function (event) {
+        event.preventDefault();
+        var email = $("#sign_up_email").val();
+        var password = $("#sign_up_password").val();
 
-}
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(
+            function (user) {
+                console.log(user);
+            }
+        )
+        .catch(
+            function (err) {
+                console.log(err);
+            }
+        )
+    })
+
+    function addInputSubmitEvent(form, input) {
+        input.onkeydown = function(e) {
+            e = e || window.event;
+            if (e.keyCode == 13) {
+                form.submit();
+                return false;
+            }
+        };
+    }
+});
